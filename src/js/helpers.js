31d22386db797c520835d1d951f45e26e2aea3ce
Wellington Ivo <wellingtonivoprog@gmail.com>
2022-04-13 23:45:41 -0300

    add popup feature, refactor JS code, add popup-windows.css file

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -5,3 +5,31 @@ export const removeHidden = function (element) {
 export const addHidden = function (element) {
   element.classList.add("hidden");
 };
+
+export const popupMarkup = `
+<div class="new-manga-popup">
+              <div class="popup-manga-img-title">
+                <img src="./src/img/sushi placeholder.webp" alt="Manga Cover" class="new-manga-cover-img">
+                <span class="popup-manga-title">Manga title ### # ### Manga title</span>
+              </div>
+              <div class="new-manga-info">
+                <div class="rate">
+                  <p class="rates">
+                    <span class="stars">&starf; &starf; &starf; &starf; &star;</span>
+                    <span class="rating">4.0</span>
+                  </p>
+                </div>
+                <div class="new-manga-description grid grid--2-cols">
+                  <span class="popup-question">Rank</span>
+                  <p class="popup-answer">XX position, it has XX monthly views</p>                    <span class="popup-question">Alternative</span>
+                  <p class="popup-answer">Alternative manga name</p>
+                  <span class="popup-question">Author(s)</span>
+                  <p class="popup-answer">Author(s) name(s)</p>
+                  <span class="popup-question">Grenre(s)</span>
+                  <p class="popup-answer">Manga genre(s) Genre#1 Genre#2</p>
+                  <span class="popup-question">Summary</span>
+                  <p class="popup-answer">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt soluta nulla accusantium, officia animi minima quod iure voluptatum harum! Culpa perferendis molestias consectetur. Error a distinctio voluptatibus possimus maxime recusandae!</p>
+                </div>
+              </div>
+            </div>
+            `;
--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,42 @@
-import { removeHidden, addHidden } from "./helpers.js";
+import { removeHidden, addHidden, popupMarkup } from "./helpers.js";
 
 // Element Selectors
 const moreListEl = document.querySelector(".more-list");
 const moreListItemsEl = document.querySelector(".more-list-items");
+const newMangaCoverImgAllEl = document.querySelectorAll(".new-manga-cover-img");
 
+///////////////////////
 const moreMenu = function () {
-  moreListEl.addEventListener("mouseover", function () {
+  moreListEl.addEventListener("mouseenter", function () {
     removeHidden(moreListItemsEl);
   });
-  moreListEl.addEventListener("mouseout", function () {
+  moreListEl.addEventListener("mouseleave", function () {
     addHidden(moreListItemsEl);
   });
-  moreListItemsEl.addEventListener("mouseout", function () {
+  moreListItemsEl.addEventListener("mouseleave", function () {
     addHidden(moreListItemsEl);
   });
 };
 
+const popup = function () {
+  newMangaCoverImgAllEl.forEach((coverImgEl) => {
+    const closestNewManga = coverImgEl.closest(".new-manga");
+
+    coverImgEl.addEventListener("mouseenter", () => {
+      closestNewManga.insertAdjacentHTML("beforeend", popupMarkup);
+    });
+
+    coverImgEl.addEventListener("mouseleave", () => {
+      const popupEl = document.querySelector(".new-manga-popup");
+      closestNewManga.removeChild(popupEl);
+    });
+  });
+};
+
+///////////////////////////
 const init = function () {
   moreMenu();
+  popup();
 };
 
 init();
